a4e30da88a5145b2c9db246e71f372d6aae1a186
Neelkanth Tandel <tandelneelkanth@gmail.com>
2022-01-25 11:35:55 +0530

    Integrated logIn API

--- Web App/src/App.js
+++ Web App/src/App.js
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import logo from "./images/icon.png";
 
 function App() {
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log(event.target.userId.value);
+    console.log(event.target.password.value);
+    const response = await fetch("http://localhost:5000/auth/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        teacherId: event.target.userId.value,
+        password: event.target.password.value,
+      }),
+    });
+    const data = await response.json();
+    console.log("User: ", data.user.name);
+  };
+
   return (
     <div className="container">
       <img src={logo} alt="logo" className="logo" />
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleLogin}>
         <input
-          name="user-id"
+          name="userId"
           type={"text"}
           placeholder="User Id"
           className="user-id-input"
@@ -20,7 +38,7 @@ function App() {
           className="password-input"
         />
         <input
-          name="login-btn"
+          name="loginBtn"
           type={"submit"}
           value="Login"
           className="login-btn"
