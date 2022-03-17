d50fd35ebb80353a11d2805a565e75c893c91a67
Hetvi Soni <hetvisoni4444@gmail.com>
2022-03-17 20:41:56 +0530

    RemoveModal

--- Web App/src/routes/Teacher.js
+++ Web App/src/routes/Teacher.js
@@ -31,6 +31,10 @@ import { com_name } from "../keys";
 const cookies = new Cookies();
 
 const Row = (props) => {
+  const [modal, setModal] = React.useState(false);
+  const toggleModal = () => {
+    setModal(!modal);
+  };
   return (
     <div
       style={{
@@ -38,11 +42,98 @@ const Row = (props) => {
       }}
       className="row-tT"
     >
-      <span style={{ width: "10%" }}>{props.id}</span>
-      <span style={{ width: "30%" }}>{props.name}</span>
-      <span style={{ width: "30%" }}>{props.mail_id}</span>
-      <span style={{ width: "20%" }}>{props.phone_number}</span>
-      <span style={{ width: "10%" }}>{props.class_id}</span>
+      <span style={{ width: "10%", minWidth: "65px" }}>{props.id}</span>
+      <span style={{ width: "50%", minWidth: "180px" }}>{props.name}</span>
+      <span style={{ width: "15%", minWidth: "65px" }}>{props.className}</span>
+      <span style={{ width: "15%", minWidth: "65px" }}>{props.div}</span>
+      <span
+        style={{
+          width: "20%",
+          justifyContent: "flex-end",
+          display: "flex",
+          minWidth: "100px",
+        }}
+      >
+        <span onClick={toggleModal} className="remove-button">
+          Remove
+        </span>
+        {modal ? (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+              {/* <div className="Name-input">
+                <b>Name</b>: {props.name}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flex: "row",
+                  marginTop: "5px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div className="Id-input">ID : {props.id}</div>
+                <div className="ClassDiv-input">
+                  ClassID : {props.className + props.div}
+                </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flex: "row",
+                  marginTop: "5px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div className="Email-input">Mail ID : {props.mail_id}</div>
+                <div className="Phone-input">
+                  Phone Number : {props.phone_number}
+                </div>
+              </div>
+              <div className="Address-input" style={{ marginTop: "5px" }}>
+                Address : {props.address}
+              </div> */}
+
+              <div className="detail">
+                <b>Name: </b>
+                {props.name}
+              </div>
+              {/* <div className="detail-container"> */}
+              <div className="detail">
+                <b>Id: </b>
+                {props.id}
+              </div>
+              <div className="detail">
+                <b>Class: </b>
+                {props.className + props.div}
+              </div>
+              {/* </div> */}
+              <div className="detail">
+                <b>Mail: </b>
+                {props.mail_id}
+              </div>
+              <div className="detail">
+                <b>Phone no.: </b>
+                {props.phone_number}
+              </div>
+              <div className="detail">
+                <b>Address: </b>
+                {props.address}
+              </div>
+              <span
+                className="saveModal-button"
+                // value={newTeacher}
+                // onChange={setNewTeacher(newTeacher)}
+                onClick={() => {
+                  setModal(!modal);
+                }}
+              >
+                Remove
+              </span>
+            </div>
+          </div>
+        ) : null}
+      </span>
     </div>
   );
 };
@@ -175,21 +266,35 @@ export default function Teacher() {
           />
           <div className="tableT">
             <div className="table-headerT">
-              <span style={{ width: "10%", fontWeight: "bold", fontSize: 18 }}>
+              <span
+                style={{
+                  minWidth: "65px",
+                  width: "10%",
+                  fontWeight: "bold",
+                  fontSize: 18,
+                }}
+              >
                 Id
               </span>
-              <span style={{ width: "30%", fontWeight: "bold", fontSize: 18 }}>
+              <span
+                style={{
+                  minWidth: "180px",
+                  width: "50%",
+                  fontWeight: "bold",
+                  fontSize: 18,
+                }}
+              >
                 Name
               </span>
-              <span style={{ width: "30%", fontWeight: "bold", fontSize: 18 }}>
-                Mail Id
+              <span style={{ width: "15%", fontWeight: "bold", fontSize: 18 }}>
+                Class
               </span>
-              <span style={{ width: "20%", fontWeight: "bold", fontSize: 18 }}>
-                Phone Number
+              <span style={{ width: "15%", fontWeight: "bold", fontSize: 18 }}>
+                Div
               </span>
-              <span style={{ width: "10%", fontWeight: "bold", fontSize: 18 }}>
-                Class ID
-              </span>
+              <span
+                style={{ width: "20%", fontWeight: "bold", fontSize: 18 }}
+              />
               {/* <table>
               <thead className="table-headerT">
                 <div className="table-headerT">
@@ -220,9 +325,11 @@ export default function Teacher() {
                 <Row
                   id={data.teacher_id}
                   name={data.name}
+                  className={data.className}
+                  div={data.div}
                   mail_id={data.mail_id}
                   phone_number={data.phone_number}
-                  class_id={data.class_id}
+                  address={data.address}
                   key={index}
                   islast={index == filteredDet.length - 1 ? true : false}
                 />
